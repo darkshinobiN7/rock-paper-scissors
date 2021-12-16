46e8b4e57d4051325d27350a245c73ebb370dff0
DarkShinobi <vagnerma@gmail.com>
2021-12-16 12:19:02 +0000

    Final files for Rock Paper Scissors UI

--- game.js
+++ game.js
@@ -1,74 +1,119 @@
 // Script for The Odin Project  - Rock, Paper and Scissors Game
 
-//Initiating the Computer's AI
+let playerScore = 0;
+let computerScore = 0;
+const playerScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+const playerImage = document.querySelector(".userChoice");
+const computerImage = document.querySelector(".computerChoice");
 
-function computerPlay() {
-    let optionList = ['rock', 'paper', 'scissors'];
-    let randomChoice = optionList[Math.floor(Math.random() * optionList.length)];
-    return randomChoice;
+function genComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
 }
 
-//Running a game between human and computer
+function win(playerChoice, computerChoice) {
+    playerScore++;
+    playerScore_span.innerHTML = playerScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `Your ${playerChoice} beats ${computerChoice}. You Win!`;
+}
 
-function playRound(humanChoice, computerChoice) {
+function lose(playerChoice, computerChoice) {
+    computerScore++;
+    playerScore_span.innerHTML = playerScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `Your ${playerChoice} loses to ${computerChoice}. You Lose!`;
+}
 
-    var humanChoice = prompt("Choose between Rock, Paper or Scissors: ").toLowerCase();
-    var computerChoice = computerPlay();
+function draw(playerChoice, computerChoice) {
+    playerScore_span.innerHTML = playerScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `Your ${playerChoice} equals to ${computerChoice}. It's a Draw!`;
+}
 
 
-    // Human goes with Rock
 
-    if (humanChoice === 'rock') {
-        switch (computerChoice) {
-            case 'rock':
-                return 'T';
-            case 'paper':
-                return 'M';
-            case 'scissors':
-                return 'Y';
-        }
+function game(playerChoice) {
+    const computerChoice = genComputerChoice();
+    switch (playerChoice + computerChoice) {
+        case "PaperRock":
+        case "RockScissors":
+        case "ScissorsPaper":
+            win(playerChoice, computerChoice);
+            break;
+        case "PaperScissors":
+        case "RockPaper":
+        case "ScissorsRock":
+            lose(playerChoice, computerChoice);
+            break;
+        case "PaperPaper":
+        case "RockRock":
+        case "ScissorsScissors":
+            draw(playerChoice, computerChoice);
+            break;
+    }
+    showSelection(playerChoice, computerChoice);
+    endGame();
 
-    // Human goes with Paper
 
-    } else if (humanChoice === 'paper') {
-        switch (computerChoice) {
-            case 'rock':
-                return 'Y';
-            case 'paper':
-                return 'T';
-            case 'scissors':
-                return 'M';
-        }
+}
 
-    // Human goes with Scissors
+function showSelection(playerChoice, computerChoice) {
+    if (playerChoice === 'Rock') {
+        playerImage.style.backgroundImage = "url('images/rock.png')";
+    } 
+    else if (playerChoice === 'Paper') {
+        playerImage.style.backgroundImage = "url('images/paper.png')";
+    }
+    else {
+        playerImage.style.backgroundImage = "url('images/scissors.png')";
+    }
+
+    if (computerChoice === 'Rock') {
+        computerImage.style.backgroundImage = "url('images/rock.png')";
+    } 
+    else if (computerChoice === 'Paper') {
+        computerImage.style.backgroundImage = "url('images/paper.png')";
+    }
+    else {
+        computerImage.style.backgroundImage = "url('images/scissors.png')";
+    }
+}
+
+function core() {
+
+    rock_div.addEventListener('click', function() { 
+        game('Rock');
     
-    } else if (humanChoice === 'scissors') {
-        switch (computerChoice) {
-            case 'rock':
-                return 'M';
-            case 'paper':
-                return 'Y';
-            case 'scissors':
-                return 'T';
-        }
+    })
+    
+    paper_div.addEventListener('click', function() { 
+        game('Paper');
+    
+    })
+    
+    scissors_div.addEventListener('click', function() { 
+        game('Scissors');
+    
+    })
+}
+
+function endGame() {
+    const disableButtons = document.getElementById("buttons");
+    if (playerScore === 5) {
+        disableButtons.classList.add("disabledButton");
+        result_p.innerHTML = "Congratulations! You are the Winner.";
+    } 
+    if (computerScore === 5) {
+        disableButtons.classList.add("disabledButton");
+        result_p.innerHTML = "It seems the Computer got the best this time. You lose!";
     }
 }
 
-// Play the game for 5 rounds and provide outcome
-
-function game() {
-    let i = 0;
-    for (i = 1; i <= 5; i++) {
-        let result = playRound()
-        if (result === 'M') {
-            return 'You lose! Try again.';
-        } else if (result === 'Y') {
-            return 'You win! Congratulations.'
-        } else if (result === 'T') {
-            return 'You win! Congratulations.'
-        }
-    }
-}        
-
-game();
-
+core();
